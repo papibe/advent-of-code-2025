f6f53a6efda9c9f4f40cc4299f74f1adab7b4bc1
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-12-07 19:56:11 -0600

    Add all python files for day 2 part 1

---
+++ python/day02/part1.py
@@ -0,0 +1,45 @@
+from collections import namedtuple
+from typing import List
+
+Range = namedtuple("Range", ["start_id", "end_id"])
+
+
+def parse(filename: str) -> List[Range]:
+    with open(filename, "r") as fp:
+        data: str = fp.read()
+
+    ranges: List[Range] = []
+    for id_range in data.split(","):
+        ids = id_range.split("-")
+        start_id: int = int(ids[0])
+        end_id: int = int(ids[1])
+
+        ranges.append(Range(start_id, end_id))
+
+    return ranges
+
+
+def solve(ranges: List[Range]) -> int:
+    sum_of_invalid_ids: int = 0
+
+    for id_range in ranges:
+        for product_id in range(id_range.start_id, id_range.end_id + 1):
+            str_id: str = str(product_id)
+            n: int = len(str_id)
+
+            if n % 2 == 0:
+                half: str = str_id[: n // 2]
+                if str_id == half + half:
+                    sum_of_invalid_ids += product_id
+
+    return sum_of_invalid_ids
+
+
+def solution(filename: str) -> int:
+    ranges: List[Range] = parse(filename)
+    return solve(ranges)
+
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # 1227775554
+    print(solution("./input.txt"))  # 29940924880
