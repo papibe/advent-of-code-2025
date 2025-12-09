afda93a4121dbedc01c1d62d2f726189dab45d75
Pablo Piaggio <Pablo.Piaggio@gmail.com>
2025-12-08 22:56:10 -0600

    Add python solution for day 2 part 2

---
+++ python/day02/part2_v1_re.py
@@ -0,0 +1,50 @@
+import re
+from collections import namedtuple
+from typing import List, Match, Optional
+
+Range = namedtuple("Range", ["start_id", "end_id"])
+
+
+def parse(filename: str) -> List[Range]:
+    with open(filename, "r") as fp:
+        data: str = fp.read()
+
+    ranges: List[Range] = []
+    for id_range in data.split(","):
+        ids = id_range.split("-")
+        start_id: int = int(ids[0])
+        end_id: int = int(ids[1])
+
+        ranges.append(Range(start_id, end_id))
+
+    return ranges
+
+
+def solve(ranges: List[Range]) -> int:
+    sum_of_invalid_ids: int = 0
+
+    for id_range in ranges:
+        for product_id in range(id_range.start_id, id_range.end_id + 1):
+            str_id: str = str(product_id)
+
+            for pattern_len in range(1, len(str_id) // 2 + 1):
+                if len(str_id) % pattern_len != 0:
+                    continue
+
+                pattern: str = f"^({str_id[:pattern_len]})" + r"{2,}$"
+                matches: Optional[Match[str]] = re.match(pattern, str_id)
+                if matches:
+                    sum_of_invalid_ids += product_id
+                    break
+
+    return sum_of_invalid_ids
+
+
+def solution(filename: str) -> int:
+    ranges: List[Range] = parse(filename)
+    return solve(ranges)
+
+
+if __name__ == "__main__":
+    print(solution("./example.txt"))  # 4174379265
+    print(solution("./input.txt"))  # 48631958998
